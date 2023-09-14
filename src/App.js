74727628ea0fcc3f0600779f4e1490403ca8dc4b
Mustafa <mustafamengutayy@gmail.com>
2023-09-14 16:46:56 +0300

    feat: create pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Explore from './pages/Explore'
+import Offers from './pages/Offers';
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import ForgotPassword from './pages/ForgotPassword'
+
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <div className="App">
-      <h1>My App</h1>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Explore />} />
+          <Route exact path="/offers" element={<Offers />} />
+          <Route exact path="/profile" element={<Profile />} />
+          <Route exact path="/sign-in" element={<SignIn />} />
+          <Route exact path="/sign-up" element={<SignUp />} />
+          <Route exact path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+        <Navbar />
+      </Router>
+    </>
   );
 }
 
